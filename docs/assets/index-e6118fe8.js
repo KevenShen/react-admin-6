d271d29a9f52556d4faa1434c40e8455a6a6396b
liangweicai <18696341302@163.com>
2023-06-15 17:50:22 +0800

    🎉 feat: 发布web项目

---
+++ docs/assets/index-e6118fe8.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-437f5f79.js";import{P as o}from"./index-c17c71ea.js";import"./PlusOutlined-f79e391e.js";const s=()=>(console.log("about"),t(o,{title:"关于我",source:"关于我的信息",children:t("div",{className:"app-card chat",style:{height:"2000px"},children:"关于"})}));export{s as default};
