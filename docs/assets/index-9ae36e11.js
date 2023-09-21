48342790947c23b1b64deae1271c1f19fcb2b64d
liangweicai <18696341302@163.com>
2023-09-21 15:53:13 +0800

    🎉 feat: 发布web项目

---
+++ docs/assets/index-9ae36e11.js
@@ -0,0 +1,1 @@
+import{r as o,j as r}from"./index-5e13deeb.js";import{P as e}from"./index-efdc5248.js";import"./PlusOutlined-848eb892.js";const a=o.memo(()=>(console.log("home"),r(e,{title:"主页",source:"图表及配置信息",children:"图表及配置信息"})));export{a as default};
