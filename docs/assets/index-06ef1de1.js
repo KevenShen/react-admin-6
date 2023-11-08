862716e4ae724c1553c0e8642f94c921e2db65ab
clw <liangweicai@upchina.com>
2023-11-08 14:06:08 +0800

    删除部分依赖

--- docs/assets/index-06ef1de1.js
+++
@@ -1,7 +0,0 @@
-import { $ as s, d as n, H as e, j as a, a0 as i, a1 as o } from './index-7ec5c925.js'
-function r() {
-  const t = s()
-  return n(e, { children: [t.pathname === '/staging' && a(i, { to: '/staging/live' }), a(o, {})] })
-}
-export { r as default }
-//# sourceMappingURL=index-06ef1de1.js.map
