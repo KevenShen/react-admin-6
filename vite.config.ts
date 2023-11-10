fe5620690c9b3f48a3a4ec3cf4e003470f81c672
jiuxiangyangguang <18696341302@163.com>
2023-11-10 21:39:30 +0800

    地址修改

--- vite.config.ts
+++ vite.config.ts
@@ -54,8 +54,9 @@ export default defineConfig({
   },
   server: {
     port: 1102,
-    host: 'localhost',
+    host: '0.0.0.0',
     open: true,
+
     proxy: {
       // '/api': {
       //   target: 'http://116.62.37.172:1102/', // easymock
